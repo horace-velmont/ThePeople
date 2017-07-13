a8ad82c9807d2b3b2eb60ef20f63c3fd6a5b7600
HoraceVelmont <dragond7@naver.com>
2017-07-13 20:00:30 +0900

    #3 Add Test Case Related in Resource Loading

--- core/src/main/java/web/game/test/DummyPeopleGame.java
+++ core/src/main/java/web/game/test/DummyPeopleGame.java
@@ -1,25 +1,33 @@
 package web.game.test;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.assets.loaders.resolvers.ClasspathFileHandleResolver;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.utils.GdxNativesLoader;
 import lombok.Getter;
+import lombok.Setter;
 import org.mini2Dx.core.assets.FallbackFileHandleResolver;
 import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
+import org.mockito.Mockito;
 
 /**
  * Created by Velmont on 2017-07-08.
  */
 public class DummyPeopleGame extends BasicGame {
-    @Getter
+    @Getter@Setter
     private AssetManager assetManager;
 
     @Override
     public void initialise() {
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+        GdxNativesLoader.load();
         FileHandleResolver fileHandleResolver = new FallbackFileHandleResolver(new ClasspathFileHandleResolver(), new InternalFileHandleResolver());
-        assetManager = new AssetManager(fileHandleResolver);
+        assetManager = new AssetManager();
     }
 
     @Override
--- core/src/main/java/web/game/test/assets/PeopleAssetLoaderTest.java
+++ core/src/main/java/web/game/test/assets/PeopleAssetLoaderTest.java
@@ -2,31 +2,38 @@ package web.game.test.assets;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.TextureAtlasLoader;
+import com.badlogic.gdx.assets.loaders.resolvers.ClasspathFileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.backends.headless.HeadlessMini2DxGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import lombok.Synchronized;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mini2Dx.core.assets.FallbackFileHandleResolver;
 import org.mini2Dx.headless.HeadlessMini2DxConfig;
 import org.mockito.Mockito;
 import web.game.people.assets.PeopleAssetLoader;
 import web.game.test.DummyPeopleGame;
 import web.game.test.gameData.UserDataTest;
 
+import javax.xml.soap.Text;
+
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 /**
  * Created by Velmont on 2017-07-13.
  */
 public class PeopleAssetLoaderTest {
-    private DummyPeopleGame  dummyPeopleGame;
+    private DummyPeopleGame dummyPeopleGame;
     private final String SAMPLE_ASSET_DIRECTORY = "texture/animation";
     private final String SAMPLE_ASSET_NAME = "magi_victory";
     private final String SAMPLE_ASSET_SUFFIX = "atlas";
@@ -36,7 +43,6 @@ public class PeopleAssetLoaderTest {
     public void setUp() throws Exception{
         dummyPeopleGame = new DummyPeopleGame();
         HeadlessMini2DxConfig config = new HeadlessMini2DxConfig(PeopleAssetLoader.class.getName());
-        Gdx.gl = mock(GL20.class);
         config.runGame = false;
         new HeadlessMini2DxGame(dummyPeopleGame, config);
     }
@@ -50,11 +56,23 @@ public class PeopleAssetLoaderTest {
     public void testIsSuffixMethodUsingRight(){
         FileHandle[] atlasFileArray = Gdx.files.internal(SAMPLE_ASSET_DIRECTORY).list(SAMPLE_ASSET_SUFFIX);
         String atlasFilePath = null;
-        for(int i = 0; i< atlasFileArray.length; i++){
+        for(int i = 0; i< atlasFileArray.length; i++)   {
             if(!atlasFileArray[i].isDirectory()){
                 atlasFilePath = atlasFileArray[i].path();
             }
         }
         assertThat(atlasFilePath, is(SAMPLE_ASSET_PATH));
     }
+
+    @Test
+    public void testLoadAtlas(){
+        AssetManager assetManager = dummyPeopleGame.getAssetManager();
+        //Before Loading
+        assertFalse(assetManager.isLoaded(SAMPLE_ASSET_PATH, TextureAtlas.class));
+
+        //After Loading
+        PeopleAssetLoader.loadAtlas(assetManager);
+        assertTrue(assetManager.isLoaded(SAMPLE_ASSET_PATH, TextureAtlas.class));
+    }
+
 }
